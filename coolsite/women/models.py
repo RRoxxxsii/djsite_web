384ab38258c3273da8d98e579f713b9bc85bbee2
RRoxxxsii <mishabur38@yandex.com>
2023-03-10 17:56:07 +0300

    updating

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Women(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
     content = models.TextField(blank=True, verbose_name='Текст статьи')
-    photo = models.ImageField(upload_to='photos/%Y/%n/%d/', verbose_name='Фото')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
